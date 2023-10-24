db8756d3d93062bd7745e7cc538d14a46bd88eab
ds2259 <66865171+ds2259@users.noreply.github.com>
2023-10-25 02:02:22 +0530

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,66 @@
+import { defaults } from 'lodash-es'
+import Browser from 'webextension-polyfill'
+
+export enum EnableMode {
+  Off = 0,
+  On = 1,
+}
+
+const userConfigWithDefaultValue = {
+  on: EnableMode.On,
+  subject: EnableMode.On,
+}
+
+export enum ProviderType {
+  ChatGPT = 'chatgpt',
+  GPT3 = 'gpt3',
+}
+
+interface GPT3ProviderConfig {
+  model: string
+  apiKey: string
+}
+
+export interface ProviderConfigs {
+  provider: ProviderType
+  configs: {
+    [ProviderType.GPT3]: GPT3ProviderConfig | undefined
+  }
+}
+
+export type UserConfig = typeof userConfigWithDefaultValue
+
+export async function getUserConfig(): Promise<UserConfig> {
+  const result = await Browser.storage.local.get(Object.keys(userConfigWithDefaultValue))
+  return defaults(result, userConfigWithDefaultValue)
+}
+
+export async function updateUserConfig(updates: Partial<UserConfig>) {
+  return Browser.storage.local.set(updates)
+}
+
+export async function onChanged(callBack) {
+  Browser.storage.local.onChanged.addListener(callBack);
+}
+
+export async function getProviderConfigs(): Promise<ProviderConfigs> {
+  const { provider = ProviderType.ChatGPT } = await Browser.storage.local.get('provider')
+  const configKey = `provider:${ProviderType.GPT3}`
+  const result = await Browser.storage.local.get(configKey)
+  return {
+    provider,
+    configs: {
+      [ProviderType.GPT3]: result[configKey],
+    },
+  }
+}
+
+export async function saveProviderConfigs(
+  provider: ProviderType,
+  configs: ProviderConfigs['configs'],
+) {
+  return Browser.storage.local.set({
+    provider,
+    [`provider:${ProviderType.GPT3}`]: configs[ProviderType.GPT3],
+  })
+}
